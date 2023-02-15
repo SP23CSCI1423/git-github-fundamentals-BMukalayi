7d504536fa3c14dee19b2df256ec64eafb47dd07
BMukalayi <123584542+BMukalayi@users.noreply.github.com>
2023-02-15 16:48:43 +0000

    mynotesfeb15

---
+++ loop.pl
@@ -0,0 +1,17 @@
+
+#For Loop 
+#for (initial value; conditioin you want to meet; increment) {
+for ($a = 1; $a <=5; $a++){
+print("for loop example: $a\n");
+}
+
+#$a++ is he same as saying $a = $a +1; $a++ increases the value by 1 with each loop
+$num1=230;
+for ($i=1; $i <=5; $i++){
+#add 230 and 360; add 230 to each result
+$num1+=360;
+print("Result: $num1\n");
+#Each time the loop runs , 360 is added to the num1m result is saved as num1, runs print statement
+}
+
+
---
+++ loopscondition.pl
@@ -0,0 +1,24 @@
+#loops: for, Foreach, while , until
+#conditionals: If, il else, if elsif else, unless
+#loopscondition
+#While Loop
+$count = 1;
+While ($count < 10) {
+print("the count is : $count\n");
+$count = $count + 1;
+}
+
+#UNtil loop
+$num = 0;
+until ($num  = = 15) {
+print("until loop example number: $num\n");
+$num =$num +1;
+}
+While Loop: Loops /while something is true
+#until loop: Loops until something is true
+#For Loop
+#for (initial value;condition you want to meet; increment) {}
+for ($a = 1; $a < = 5; $a++){
+print("For loop example : $a\n");
+}
+
