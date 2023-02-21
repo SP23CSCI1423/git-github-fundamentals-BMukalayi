d3f7b0e33c55197a16b6b4b4bc500d38251044a6
BMukalayi <123584542+BMukalayi@users.noreply.github.com>
2023-02-21 07:08:27 +0000

    savingDesmospl

---
+++ Desmos.pl
@@ -0,0 +1,13 @@
+#If else Statement
+#Ask the user for two numbers
+#Ask the user what math operation they want to perform (addition, subtraction, multiplication, division, exponentiation, or modulus)
+#Using an if-else statement, perform the operation they choose and print the results.
+
+print ("Give me a number");
+$number1= <>;
+chomp($number1);
+print ("Give me another number");
+$number2= <>;
+chomp($number2);
+print("The number you chose were $number1 and $number2\n");
+
