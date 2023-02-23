4c46030d6f8257bf5a5009236cb952ade77a14a0
BMukalayi <123584542+BMukalayi@users.noreply.github.com>
2023-02-23 06:46:54 +0000

    thirdtrialDesmospl

--- Desmos.pl
+++ Desmos.pl
@@ -10,12 +10,6 @@ print ("Give me another number");
 $number2= <>;
 chomp($number2);
 print("The number you chose were $number1 and $number2\n");
-$add1 = $number1 + $number2;
-$sub1 = $number1 - $number2;
-$mult1 = $number1 * $number2;
-$div1 = $number1 / $number2;
-$expo1 = $number1 ** $number2;
-$mod1 = $number1 % $number2;
 print("What operation would you like to perform?\n");
 $response = <>;
 Chomp($response);
@@ -28,16 +22,20 @@ elsif($response eq "substraction"){
   $sub1 = $number1 - $number2;
   print("Substraction is : $sub1\n");
 }
-elsif($response eq "multiplication"){
+elsif($response eq "multiplication"
+){
 $mult1 = $number1 * $number2;
  print("multiplication is: $mult1\n");
  }
  elsif($response eq "division\n"){
+  $div1 = $number1 / $number2;
   print("division is : $div1\n");
  }
 elsif($response eq "exponent\n"){
+  $expo1 = $number1 ** $number2;
   print("exponent is : $exp1\n");
 }
  else{
+  $mod1 = $number1 % $number2;
       print("modulus is :$mod1\n");
 }
