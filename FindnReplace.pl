bebcdd1028d6f856f83df7505a662fb41cf6de0d
BMukalayi <123584542+BMukalayi@users.noreply.github.com>
2023-03-07 04:48:03 +0000

    labMarch06FindnReplace

---
+++ FindnReplace.pl
@@ -0,0 +1,12 @@
+#Name the lab FindnReplace.pl
+#Create 3 strings, each with different topics (food, cars, colors, etc.)
+#For the first string, use matching and print if there is a match (be sure to have if else statement)
+#For the second string, pick one word and replace it with another
+#For the third string, use the translation function to replace one letter with another
+
+#***Be sure to print the original string and then the updated string (so after a word or letter has been replaced)
+
+#First String
+@Africanfood = ("Madeso","Mchelee", "Samaki", "Ugali");
+print("Firs string is :@Africanfood \n");
+
